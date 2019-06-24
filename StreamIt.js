f03488f29b835c5f38b137d5475c31121e79fd2b
LejlaDolovac <Llleyla_@hotmail.com>
2019-06-24 12:53:26 +0200

    first update

---
+++ StreamIt.js
@@ -0,0 +1,37 @@
+
+function show_alert(){
+       console.log("hej");
+        return confirm(" Yeey your message is sent!");
+   }
+
+   /*
+function changeColor(){
+    console.log("hejehj");
+    document.getElementById("text");
+    text.style.color = "red";
+}   
+
+
+function changeColor(){
+    console.log('hejhej');
+if(changeColor())
+document.getElementById("text").innerHTML;
+text.style.color = "red";
+}
+
+
+
+
+
+var header = document.getElementsByClassName("nav-bar");
+
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+*/
